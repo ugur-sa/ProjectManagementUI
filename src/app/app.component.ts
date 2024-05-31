87ef5b7303442bfd9aa124530bce06cc6e48cbd4
Ugur Sadiklar <sadiklarugur@gmail.com>
2024-05-31 16:33:42 +0200

    Add api connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Task } from './models/task';
+import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ProjectManagementUI';
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe(tasks => {
+      this.tasks = tasks;
+    });
+  }
 }
---
+++ src/app/models/task.ts
@@ -0,0 +1,8 @@
+export class Task {
+  id = '';
+  description = '';
+  title = '';
+  created = new Date();
+  updated = new Date();
+  due = new Date();
+}
