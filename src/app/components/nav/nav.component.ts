7fe9025de5a4ac0292a5ebfe987af543412e2c21
Ugur Sadiklar <sadiklarugur@gmail.com>
2024-06-04 17:45:39 +0200

    Fix router-outlet layout

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
 })
 export class NavComponent {
