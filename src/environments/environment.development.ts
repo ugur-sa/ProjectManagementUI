66207985b762345c7947ac151bf15262922bf2ad
Ugur Sadiklar <sadiklarugur@gmail.com>
2024-06-04 13:57:03 +0200

    Push environments to repo

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:7167/api',
+  realWorldAPI: 'https://api.realworld.io/api',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://localhost:7167/api',
+  realWorldAPI: 'https://api.realworld.io/api',
+};
