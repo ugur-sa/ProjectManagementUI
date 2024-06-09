e515808d93f0b74181eb2a047f2a1fdf06d504da
ugur-sa <sadiklarugur@gmail.com>
2024-06-09 02:30:54 +0200

    Rearrange paths to reload same route on refresh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,24 @@ import { LoginComponent } from './features/auth/components/login/login.component
 import { RegistrationComponent } from './features/auth/components/registration/registration.component';
 import { DashboardComponent } from './pages/projects/components/dashboard/dashboard.component';
 import { isLoggedInGuard } from './core/guards/is-logged-in.guard';
+import { WorkItemsComponent } from './pages/projects/components/work-items/work-items.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full',
-  },
   {
     path: 'dashboard',
     component: DashboardComponent,
     canActivate: [isLoggedInGuard],
   },
+  {
+    path: 'work-items',
+    component: WorkItemsComponent,
+    canActivate: [isLoggedInGuard],
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
+  {
+    path: '',
+    redirectTo: '/work-items',
+    pathMatch: 'full',
+  },
 ];
