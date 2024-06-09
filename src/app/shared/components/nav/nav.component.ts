dfd83f6cdc817317f7722b111a645cd90c55809f
ugur-sa <sadiklarugur@gmail.com>
2024-06-09 17:35:26 +0200

    Move logout to nav menu and open dialog with settings button

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,20 +1,46 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../features/auth/services/auth.service';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatDialog } from '@angular/material/dialog';
+import { UserSettingsDialogComponent } from './components/user-settings-dialog/user-settings-dialog.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatIconButton,
+    MatMenuTrigger,
+    MatMenu,
+    MatMenuItem,
+    MatIcon,
+    MatButton,
+  ],
   templateUrl: './nav.component.html',
 })
 export class NavComponent {
+  @ViewChild('menuTrigger') menuTrigger: MatMenuTrigger;
+
   authService = inject(AuthService);
   router = inject(Router);
+
+  constructor(public dialog: MatDialog) {}
 
   getInitial(): string {
     return (
       this.authService.currentUserSig()?.username.charAt(0).toUpperCase() ?? ''
     );
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(UserSettingsDialogComponent, {
+      restoreFocus: false,
+    });
+
+    dialogRef.afterClosed().subscribe(() => this.menuTrigger.focus());
   }
 }
